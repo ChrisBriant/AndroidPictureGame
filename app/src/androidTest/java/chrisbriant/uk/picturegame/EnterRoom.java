c2d8fd8bc03f829118b6646026c12e16e1692f31
Your Name <cbriant@btinternet.com>
2021-04-22 07:15:58 +0100

    Added some unit testing

---
+++ app/src/androidTest/java/chrisbriant/uk/picturegame/EnterRoom.java
@@ -0,0 +1,72 @@
+package chrisbriant.uk.picturegame;
+
+import android.util.Log;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.nio.charset.Charset;
+import java.util.Random;
+
+import chrisbriant.uk.picturegame.activities.MainActivity;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class EnterRoom {
+
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void enterName() throws InterruptedException {
+        //Enter name
+        onView(withId(R.id.enterNameTxt)).perform((clearText()))
+                .perform(typeText("Engineer"))
+                .perform(closeSoftKeyboard());
+        onView(withId(R.id.sendNameBtn)).perform(click());
+        //Add room name
+        String generatedString = RandomString.getAlphaNumericString(8);
+
+        Log.d("Random String",generatedString);
+
+        onView(withId(R.id.rmNewRoomEdt)).perform((clearText()))
+                .perform(typeText(generatedString));
+        onView(withId(R.id.rmNewRoomEdt)).perform((clearText()))
+                .perform(typeText(generatedString));
+        //Click add room
+        onView(withId(R.id.rmAddRoomBtn)).perform(click());
+
+        //Leave open for 10 seconds after
+        Thread.sleep(10000);
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
